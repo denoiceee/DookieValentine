74138b85cfb07eba5cd3b157d68e89287abbbda2
denoiceee <garcianovianadenisse@gmail.com>
2025-02-04 21:00:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function selectOption(option) {
         });
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
+        document.getElementById('no-button').innerText = 'You sure, bink?'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
@@ -23,9 +23,9 @@ function selectOption(option) {
     }
 }
 
-// Function to flash rainbow colors and then execute a callback function
+// Function to flash baby blue and baby pink colors and then execute a callback function
 function flashRainbowColors(callback) {
-    var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
+    var colors = ['#BFEFFF', '#FFB6C1']; // Baby blue and baby pink colors
     var i = 0;
     var interval = setInterval(function() {
         document.body.style.backgroundColor = colors[i];
